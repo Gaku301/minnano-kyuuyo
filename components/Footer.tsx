42b7ea2544ccf75ba6b14923817fafc9571711e8
Gaku Ishikawa <gaku.jp.301@gmail.com>
2022-09-07 16:51:36 +0900

    install tailwindcss

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,19 +1,18 @@
 import React from "react";
 import Image from 'next/image'
-import styles from "../styles/components/Footer.module.css"
 
 const Footer = () => {
   return (
-    <footer className={styles.footer}>
+    // <footer className={styles.footer}>
+    <footer className="flex h-24 w-full items-center justify-center border-t">
       <a
+        className="flex items-center justify-center gap-2"
         href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
         target="_blank"
         rel="noopener noreferrer"
       >
         Powered by{' '}
-        <span className={styles.logo}>
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </span>
+        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
       </a>
     </footer>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,37 @@
 import type { NextPage } from 'next'
-import styles from '../styles/pages/Home.module.css'
 
 const Home: NextPage = () => {
   return (
     <>
-      <h1 className={styles.title}>
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      <h1 className="text-6xl font-bold">
+        Welcome to{" "}
+        <a className="text-blue-600" href="https://nextjs.org">Next.js!</a>
       </h1>
 
-      <p className={styles.description}>
+      <p className="mt-3 text-2xl">
         Get started by editing{' '}
-        <code className={styles.code}>pages/index.tsx</code>
+        <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">pages/index.tsx</code>
       </p>
 
-      <div className={styles.grid}>
-        <a href="https://nextjs.org/docs" className={styles.card}>
-          <h2>Documentation &rarr;</h2>
-          <p>Find in-depth information about Next.js features and API.</p>
+      <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
+        <a 
+          href="https://nextjs.org/docs" 
+          className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+        >
+          <h2 className="text-2xl font-bold">Documentation &rarr;</h2>
+          <p className="mt-4 text-xl">Find in-depth information about Next.js features and API.</p>
         </a>
 
-        <a href="https://nextjs.org/learn" className={styles.card}>
-          <h2>Learn &rarr;</h2>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
+        <a 
+          href="https://nextjs.org/learn" 
+          className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+        >
+          <h2 className="text-2xl font-bold">Learn &rarr;</h2>
+          <p className="mt-4 text-xl">Learn about Next.js in an interactive course with quizzes!</p>
         </a>
       </div>
+
+      <h2 className="font-bold text-white">ほげk</h2>
     </>
   )
 }
