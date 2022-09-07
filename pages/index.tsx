077f1bc1c1d9389395f33b077aba4b28e696cbc7
Gaku Ishikawa <gaku.jp.301@gmail.com>
2022-09-07 15:48:31 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,31 @@
+import type { NextPage } from 'next'
+import styles from '../styles/pages/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <h1 className={styles.title}>
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+
+      <p className={styles.description}>
+        Get started by editing{' '}
+        <code className={styles.code}>pages/index.tsx</code>
+      </p>
+
+      <div className={styles.grid}>
+        <a href="https://nextjs.org/docs" className={styles.card}>
+          <h2>Documentation &rarr;</h2>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+
+        <a href="https://nextjs.org/learn" className={styles.card}>
+          <h2>Learn &rarr;</h2>
+          <p>Learn about Next.js in an interactive course with quizzes!</p>
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default Home
